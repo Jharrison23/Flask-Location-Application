2e982bc706e8ef0685aa45728ae39cf46103decd
James Harrison <jharrison23@knights.ucf.edu>
2017-03-08 12:27:02 -0500

    Updated code to match tutorial

--- routes.py
+++ routes.py
@@ -4,13 +4,21 @@ from flask import Flask, render_template
 # Create and store a useable instance of the flask class
 app = Flask(__name__)
 
-# Map the URL "/" to the python function index
+# Map the URL "/" to the python function index which opens index.html
 @app.route("/")
 
 # The python function index uses the flask function render template to render index.html
 # So when a user types in the url / the function index() will run and return the page index.html
 def index():
     return render_template("index.html")
+
+# Map the URL '/about' to the python funciton about which opens about.html
+@app.route("/about")
+
+# Function to return the page about.html
+def about():
+    return render_template("about.html")
+
 
 # app.run, runs the app on a local server
 # debug true flag allows us to see any error messages
